a2d59a7dc6526bf6ccc19e827910f504d4c41caa
Amirrul Ilham <amirrul.ilham@daysmart.com>
2025-12-14 02:24:47 +0800

    Improve handling of captureMethod in PaymentSheet configuration to ensure nil safety.

--- packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/StripeSdk+PaymentSheet.swift
+++ packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/StripeSdk+PaymentSheet.swift
@@ -201,7 +201,7 @@ extension StripeSdk {
                 resolve(Errors.createError(ErrorType.Failed, "You must provide `intentConfiguration.confirmHandler` if you are not passing an intent client secret"))
                 return
             }
-            let captureMethodString = modeParams["captureMethod"] as? String
+            let captureMethodString = modeParams["captureMethod"] != nil ? (modeParams["captureMethod"] as? String) : nil
             let intentConfig = buildIntentConfiguration(
                 modeParams: modeParams,
                 paymentMethodTypes: intentConfiguration["paymentMethodTypes"] as? [String],
