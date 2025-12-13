a88bd24bfad17586366b2d686993145ccd7668c8
Amirrul Ilham <amirrul.ilham@daysmart.com>
2025-12-14 03:36:07 +0800

    Update captureMethod assignment in PaymentSheet to use modeParams for improved clarity and consistency in intent configuration.

--- packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/StripeSdk+PaymentSheet.swift
+++ packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/StripeSdk+PaymentSheet.swift
@@ -223,7 +223,7 @@ extension StripeSdk {
                 resolve(Errors.createError(ErrorType.Failed, "You must provide either `confirmHandler` or `confirmationTokenConfirmHandler`, but not both"))
                 return
             }
-            let captureMethodString = intentConfiguration["captureMethod"] as? String
+            let captureMethodString = modeParams["captureMethod"] as? String
             let intentConfig = buildIntentConfiguration(
                 modeParams: modeParams,
                 paymentMethodTypes: intentConfiguration["paymentMethodTypes"] as? [String],
